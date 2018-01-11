ef2fe75fe6abb64841208786be510166d92deddb
li.li <li.li@liulishuo.com>
2018-01-11 17:21:42 +0800

    alpha.1

--- src/commands/create.js
+++ src/commands/create.js
@@ -17,7 +17,7 @@ export default {
   builder: {
     repo: {
       description: 'template repo',
-      default: 'git@git.llsapp.com:li.li/lls-cli-template.git'
+      default: 'git@git.llsapp.com:li.li/lls-cli-template-default.git'
     },
     name: {
       description: 'project name'
